3ce4f85a74d192a57cd8da36a733a2f32ce1e631
mxc-maggiechen <mxc.maggiechen@gmail.com>
2023-09-17 00:05:10 -0400

    fixed deploy

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter
+import { HashRouter } from "react-router-dom"; // Import BrowserRouter
 import App from './App';
 
 ReactDOM.render(
-  <Router basename="/mxc-maggiechen.github.io"> {/* Set the basename to your repository name */}
+  <HashRouter> {/* Set the basename to your repository name */}
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </Router>,
+  </HashRouter>,
   document.getElementById('root')
 );
