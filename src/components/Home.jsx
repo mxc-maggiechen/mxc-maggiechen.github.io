5dbdb749a7822a5f756069912a432975ad9dfc43
mxc-maggiechen <mxc.maggiechen@gmail.com>
2023-09-17 20:30:24 -0400

    edited resume

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Dino from '../assets/mdi_dinosaur-pixel.png'
 import {TypeAnimation} from 'react-type-animation'
-import Resume from "../assets/files/Maggie Chen - Resume.pdf"
+import Resume from "../assets/files/2A - Maggie Chen - Resume.pdf"
 
 const Home = () => {
   return (
